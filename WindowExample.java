88fa31f351a31446eb4c3884fdc3b597387611c0
copado30 <99929998+copado30@users.noreply.github.com>
2024-12-05 20:18:35 +0000

    added button controls to the left panel

--- WindowExample.java
+++ WindowExample.java
@@ -31,12 +31,15 @@ public class WindowExample extends JFrame{
         //JLabel labelTop = new JLabel("You are a CS376 rockstar! Today:" + addTime());
 
         //1. left panel creation here
-
+        leftPanel = new JPanel();
+        leftPanel.setLayout(new BoxLayout(leftPanel,0));
         //2. bottom pane creation here
         bottomPanel= new JLayeredPane();
         bottomPanel.setPreferredSize(new Dimension(600, 600));
 
         //3. happy and sad image buttons
+        buttonHappy = new JButton(new HappyAction());
+        buttonSad = new JButton(new SadAction());
 
         //4. happy and sad button change image
 
@@ -44,12 +47,12 @@ public class WindowExample extends JFrame{
         ImageIcon imgHappy = new ImageIcon("images/smiley.png");
         JLabel imgLabelHappy = new JLabel(imgHappy);
         JScrollPane scrollPaneHappy = new JScrollPane(imgLabelHappy);
-scrollPaneHappy.setBounds(10, 50, imgHappy.getIconWidth()+10, imgHappy.getIconHeight()+10);
+        scrollPaneHappy.setBounds(10, 50, imgHappy.getIconWidth()+10, imgHappy.getIconHeight()+10);
 
-ImageIcon imgSad = new ImageIcon("images/frowny.png");
-JLabel imgLabelSad = new JLabel(imgSad);
-JScrollPane scrollPaneSad = new JScrollPane(imgLabelSad);
-scrollPaneSad.setBounds(10, 50, imgSad.getIconWidth()+10, imgSad.getIconHeight()+10);
+        ImageIcon imgSad = new ImageIcon("images/frowny.png");
+        JLabel imgLabelSad = new JLabel(imgSad);
+        JScrollPane scrollPaneSad = new JScrollPane(imgLabelSad);
+        scrollPaneSad.setBounds(10, 50, imgSad.getIconWidth()+10, imgSad.getIconHeight()+10);
         //6. OK and NOT button listeners to chanage images
 
         //7. labels with image signature 
@@ -62,15 +65,16 @@ scrollPaneSad.setBounds(10, 50, imgSad.getIconWidth()+10, imgSad.getIconHeight()
         areaPanel.add(BorderLayout.PAGE_START,topPanel);
 
         //9. add buttons Happy and Sad to left panel
-
+        leftPanel.add(buttonHappy);
+        leftPanel.add(buttonSad);
         //10. add buttons OK and NOT to left panel
 
         //11. add left panel to frame
-
+        areaPanel.add(BorderLayout.LINE_START,leftPanel);
         //12. add images to bottom panel
-bottomPanel.add(labelBottom);
-bottomPanel.add(scrollPaneHappy);
-bottomPanel.add(scrollPaneSad);
+        bottomPanel.add(labelBottom);
+        bottomPanel.add(scrollPaneHappy);
+        bottomPanel.add(scrollPaneSad);
 
         //13. add bottom panel to frame
  
@@ -88,4 +92,19 @@ bottomPanel.add(scrollPaneSad);
 }
 
 //16. Happy and Sad Action listeners implemetation 
+class HappyAction extends AbstractAction{
+  HappyAction(){ super("Good"); }
+  @Override
+  public void actionPerformed(ActionEvent arg0) {
+    System.out.println("Good");
+  }
+}
+
+class SadAction extends AbstractAction{
+  SadAction(){ super("Sad"); }
+  @Override
+  public void actionPerformed(ActionEvent arg0) {
+    System.out.println("Noooot Good");
+  }
+}
 
